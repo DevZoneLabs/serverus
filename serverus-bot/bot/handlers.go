9b5f0c926f5803963b3ae0360d2066fb323b2221
guisaez <guillesaez15@icloud.com>
2024-05-01 20:54:27 -0400

    Additional Handlers, Functions

--- serverus-bot/bot/handlers.go
+++ serverus-bot/bot/handlers.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/bwmarrin/discordgo"
@@ -11,7 +12,7 @@ const SERVER_DOWN_MESSAGE = "Sorry! I cannot your request right now!"
 func (bot *Bot) registerHandlers() {
 
 	bot.addHandler(helloWorld)
-
+	bot.addHandler(channelInfo)
 }
 
 func (bot *Bot) addHandler(handler interface{}) func() {
@@ -35,9 +36,32 @@ func (bot *Bot) addHandler(handler interface{}) func() {
 	}
 }
 
-func helloWorld(session *discordgo.Session, message *discordgo.MessageCreate){
-	
+func helloWorld(session *discordgo.Session, message *discordgo.MessageCreate) {
+
 	if message.Content == "Hello" {
 		session.ChannelMessageSend(message.ChannelID, "World!")
+	}
+}
+
+type ChannelInfo struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+}
+
+func channelInfo(session *discordgo.Session, message *discordgo.MessageCreate) {
+
+	if message.Content == "Channel Info"{
+		channelInfo, err := session.Channel(message.ChannelID)
+		if err != nil {
+			session.ChannelMessageSend(message.ChannelID, "Could not get channel information")
+			return
+		}
+
+		jsonData, _:= json.MarshalIndent(ChannelInfo{
+			ID: channelInfo.ID,
+			Name: channelInfo.Name,
+		}, "", "\t")
+
+		session.ChannelMessageSend(message.ChannelID, string(jsonData))
 	}
 }
